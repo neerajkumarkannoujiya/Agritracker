80be269ab8ecb49ca97bf2f246b734abb1fb4a9b
neerajkumarkannoujiya <neerajkumar7388011@gmail.com>
2025-04-04 10:43:24 +0530

    First Commit

--- agritracker_main/settings.py
+++ agritracker_main/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'agritracker.urls'
+ROOT_URLCONF = 'agritracker_main.urls'
 
 TEMPLATES = [
     {
